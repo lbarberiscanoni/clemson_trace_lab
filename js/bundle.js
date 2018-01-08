38bb34d0aa4ffd40836bcd38db6370b9e337df20
Rafael <starmarstrek@gmail.com>
2018-01-08 11:24:53 -0500

    Updates main page to contain Dr. McNeese's home page's information

--- js/bundle.js
+++ js/bundle.js
@@ -335,6 +335,7 @@ var Main = function (_React$Component) {
         key: "navigate",
         value: function navigate(newLocation) {
             var a = newLocation.toLowerCase();
+
             this.setState({ "location": a });
         }
     }, {
@@ -346,7 +347,111 @@ var Main = function (_React$Component) {
                         "div",
                         null,
                         _react2.default.createElement(_NavBar2.default, { changeLocation: this.navigate.bind(this) }),
-                        _react2.default.createElement("div", { className: "container" })
+                        _react2.default.createElement(
+                            "div",
+                            { className: "container" },
+                            _react2.default.createElement(
+                                "center",
+                                null,
+                                _react2.default.createElement(
+                                    "h3",
+                                    null,
+                                    "Assistant Professor of Human-Centered Computing,"
+                                ),
+                                _react2.default.createElement(
+                                    "h3",
+                                    null,
+                                    "Director of TRACE Research Group,"
+                                ),
+                                _react2.default.createElement(
+                                    "h3",
+                                    null,
+                                    "Clemson University"
+                                ),
+                                _react2.default.createElement("img", { src: "https://nathanmcneese.weebly.com/uploads/1/2/6/1/12617833/published/_7200551.jpg" })
+                            ),
+                            _react2.default.createElement("p", null),
+                            _react2.default.createElement(
+                                "p",
+                                null,
+                                "Greetings! I am an Assistant Professor and Director of the Team Research Analytics in Computational Environments (TRACE) Research Group within the division of Human-Centered Computing in the School of Computing at Clemson University. I received my PhD in Information Sciences & Technology with a focus on Team Decision Making, Cognition, and Computer Supported Collaborative Work from The Pennsylvania State University in the fall of 2014."
+                            ),
+                            _react2.default.createElement(
+                                "p",
+                                null,
+                                "After graduation, I accepted a position to be a Postdoctoral Scholar and Research Associate in Human Systems Engineering at Arizona State University working directly with Dr. Nancy Cooke."
+                            ),
+                            _react2.default.createElement(
+                                "p",
+                                null,
+                                "My research interests are wide ranging and interdisciplinary, but can be generally linked to the integration of"
+                            ),
+                            _react2.default.createElement(
+                                "em",
+                                null,
+                                _react2.default.createElement(
+                                    "ul",
+                                    null,
+                                    _react2.default.createElement(
+                                        "li",
+                                        null,
+                                        "Human Factors"
+                                    ),
+                                    _react2.default.createElement(
+                                        "li",
+                                        null,
+                                        "Human Computer Interaction"
+                                    ),
+                                    _react2.default.createElement(
+                                        "li",
+                                        null,
+                                        "Computer Supported Cooperative Work"
+                                    ),
+                                    _react2.default.createElement(
+                                        "li",
+                                        null,
+                                        "Information Sciences"
+                                    )
+                                )
+                            ),
+                            _react2.default.createElement(
+                                "p",
+                                null,
+                                "For over 10 years, I have conducted research on many topics within a variety of different contexts. My current research interests span across the study of individual and team cognition, the development and design of human-centered tools and systems, continued development and refinement of cognitive engineering methods, context specific measurement of team cognition, the interplay between team interaction and team cognition, the development and application of unmanned aerial vehicles, and human robot interaction. Specific contexts that I have conducted my research in: ",
+                                _react2.default.createElement(
+                                    "em",
+                                    null,
+                                    "healthcare systems, homeland security, command and control, emergency crisis management"
+                                ),
+                                ", and ",
+                                _react2.default.createElement(
+                                    "em",
+                                    null,
+                                    "educational learning"
+                                ),
+                                ". Throughout all of my research, I typically use multiple mixed methods to understand the context, work, and roles specific to the development of individual and team cognition. As a result of my work, my research has been published multiple times in numerous research communities."
+                            ),
+                            _react2.default.createElement(
+                                "p",
+                                null,
+                                _react2.default.createElement(
+                                    "strong",
+                                    null,
+                                    "Research Interests & Expertise:"
+                                ),
+                                " Human factors, team decision-making, human-computer interaction, computer-supported cooperative work, team cognition, cognitive science, usability research and design, collaborative system design, human-centered design, team mental models, mental models, collaboration, collaborative information seeking, cognitive task analysis, knowledge elicitation, human autonomy teaming, healthcare systems, big data, simulations, cybersecurity"
+                            ),
+                            _react2.default.createElement(
+                                "p",
+                                null,
+                                _react2.default.createElement(
+                                    "strong",
+                                    null,
+                                    "Research Methods:"
+                                ),
+                                " A/B testing, card sorting, cognitive walkthrough, concept mapping, contextual inquiry, ethnography, experimental design, focus groups, heuristic evaluation, interviewing, personas, prototyping, scenarios, surveying, task analysis, usability testing, wireframing"
+                            )
+                        )
                     );
                     break;
                 case "students":
--- jsx/main.jsx
+++ jsx/main.jsx
@@ -24,6 +24,7 @@ class Main extends React.Component {
 
     navigate(newLocation) {
         let a = newLocation.toLowerCase();
+        
         this.setState({"location": a})
     }
 
@@ -34,6 +35,26 @@ class Main extends React.Component {
                     <div>
                         <NavBar changeLocation = { this.navigate.bind(this) } />
                         <div className="container">
+                            <center>
+                                <h3>Assistant Professor of Human-Centered Computing,</h3>
+                                <h3>Director of TRACE Research Group,</h3>
+                                <h3>Clemson University</h3>
+                                <img src="https://nathanmcneese.weebly.com/uploads/1/2/6/1/12617833/published/_7200551.jpg" />
+                            </center>
+                            <p></p>
+                            
+                            <p>Greetings! I am an Assistant Professor and Director of the Team Research Analytics in Computational Environments (TRACE) Research Group within the division of Human-Centered Computing in the School of Computing at Clemson University. I received my PhD in Information Sciences & Technology with a focus on Team Decision Making, Cognition, and Computer Supported Collaborative Work from The Pennsylvania State University in the fall of 2014.</p>
+                            <p>After graduation, I accepted a position to be a Postdoctoral Scholar and Research Associate in Human Systems Engineering at Arizona State University working directly with Dr. Nancy Cooke.</p>
+                            <p>My research interests are wide ranging and interdisciplinary, but can be generally linked to the integration of</p>
+                            <em><ul>
+                              <li>Human Factors</li>
+                              <li>Human Computer Interaction</li>
+                              <li>Computer Supported Cooperative Work</li>
+                              <li>Information Sciences</li>
+                            </ul></em>
+                            <p>For over 10 years, I have conducted research on many topics within a variety of different contexts. My current research interests span across the study of individual and team cognition, the development and design of human-centered tools and systems, continued development and refinement of cognitive engineering methods, context specific measurement of team cognition, the interplay between team interaction and team cognition, the development and application of unmanned aerial vehicles, and human robot interaction. Specific contexts that I have conducted my research in: <em>healthcare systems, homeland security, command and control, emergency crisis management</em>, and <em>educational learning</em>. Throughout all of my research, I typically use multiple mixed methods to understand the context, work, and roles specific to the development of individual and team cognition. As a result of my work, my research has been published multiple times in numerous research communities.</p>
+                            <p><strong>Research Interests & Expertise:</strong> Human factors, team decision-making, human-computer interaction, computer-supported cooperative work, team cognition, cognitive science, usability research and design, collaborative system design, human-centered design, team mental models, mental models, collaboration, collaborative information seeking, cognitive task analysis, knowledge elicitation, human autonomy teaming, healthcare systems, big data, simulations, cybersecurity</p>
+                            <p><strong>Research Methods:</strong> A/B testing, card sorting, cognitive walkthrough, concept mapping, contextual inquiry, ethnography, experimental design, focus groups, heuristic evaluation, interviewing, personas, prototyping, scenarios, surveying, task analysis, usability testing, wireframing</p>
                         </div>
                     </div>
                 )
